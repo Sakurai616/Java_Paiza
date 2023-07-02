3c0b34ab65fd8237b6b023e3f9caf874602aa8b2
Sakurai616 <yyingjing82@gmail.com>
2023-07-02 23:21:21 +0900

    数値の出力

--- start_Java.java
+++ start_Java.java
@@ -3,3 +3,45 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+
+/* 演習課題「メッセージを表示しよう - その 1」*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello paiza");
+    }
+}
+
+/* 演習課題「メッセージを表示しよう - その 2」*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Good morning");
+    }
+}
+
+/* 演習課題「メッセージを表示しよう - その 3」*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("世界の皆さん、こんにちは");
+    }
+}
+
+/* 演習課題「数値を表示しよう」*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("12345");
+    }
+}
+
+/*演習課題「足し算しよう」*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(12345 + 6789);
+    }
+}
+
+/*演習課題「数式を表示しよう」*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("12345 + 6789");
+    }
+}
